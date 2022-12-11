6d40f685bf5127e60b082c632d191d486100a321
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2022-12-10 16:38:06 -0800

    Error on QA issue detection

--- api/scripts/qa.sh
+++ api/scripts/qa.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 echo 'Checking types'
 echo '=============='
-yarn run tsc --noEmit -p tsconfig.json
+yarn run tsc --noEmit -p tsconfig.json || exit 1
 echo $'\nChecking with `eslint`'
 echo '======================'
-yarn run eslint .
+yarn run eslint . || exit 1
 echo $'\nFixing style issues'
 echo '==================='
-yarn run eslint --fix .
+yarn run eslint --fix . || exit 1
 echo $'\n^ Any unresolved issues are shown above.'
