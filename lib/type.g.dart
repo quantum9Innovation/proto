644e32b6e9100325e0ce21013c14f7ef20d10570
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-02-19 19:03:34 -0800

    Create type.g.dart

---
+++ lib/type.g.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/painting.dart';
+
+const latinFonts = {
+  'display': TextStyle(
+    fontFamily: 'Inter Tight',
+    fontWeight: FontWeight.w800,
+    fontSize: 72,
+  ),
+  'display-italic': TextStyle(
+    fontFamily: 'Inter Tight',
+    fontWeight: FontWeight.w800,
+    fontStyle: FontStyle.italic,
+    fontSize: 72,
+  )
+};
