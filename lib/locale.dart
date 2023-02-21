e1f6e804cf1091f8839ba55ba28e83a9722839bc
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-02-20 20:23:23 -0800

    Add base search theme
    Still missing text styles

--- lib/locale.dart
+++ lib/locale.dart
@@ -2,8 +2,31 @@
 // The initial screen shown on first app startup
 // Used to determine user locale preferences
 
+import 'type.g.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_l10n.dart';
+
+class Search extends StatefulWidget {
+  const Search({Key? key}) : super(key: key);
+
+  @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
+  Widget build(BuildContext context) {
+    return const TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: 'Search for a language',
+        prefixIcon: Padding(
+            padding: EdgeInsets.only(left: 16, right: 16),
+            child: Icon(Icons.search)),
+      ),
+    );
+  }
+}
 
 class Locale extends StatelessWidget {
   const Locale({Key? key}) : super(key: key);
@@ -21,25 +44,24 @@ class Locale extends StatelessWidget {
       'es_MX': {
         'name': 'Español',
         'country': 'México',
-      }
+      },
     };
 
     return Scaffold(
-        appBar: AppBar(
-          elevation: 2,
-          title: Text(AppLocalizations.of(context)!.helloWorld),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                AppLocalizations.of(context)!.main,
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-            onPressed: () => {}, child: const Icon(Icons.add)));
+      body: Padding(
+          padding: const EdgeInsets.only(left: 100, top: 100),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            SizedBox(
+                height: 100,
+                child: Text(AppLocalizations.of(context)!.locale_title,
+                    style: latinFonts['display'])),
+            Container(
+              padding: const EdgeInsets.only(left: 0, top: 45),
+              width: 800,
+              child: const Search(),
+            ),
+          ])),
+    );
   }
 }
