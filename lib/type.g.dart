d4bc9c7033a8773d34f27c4cfd0c7847293b9ede
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-02-25 20:31:34 -0800

    Update type.g.dart
    Add body text

--- lib/type.g.dart
+++ lib/type.g.dart
@@ -11,5 +11,16 @@ const latinFonts = {
     fontWeight: FontWeight.w800,
     fontStyle: FontStyle.italic,
     fontSize: 72,
+  ),
+  'body': TextStyle(
+    fontFamily: 'Noto Sans',
+    fontWeight: FontWeight.w400,
+    fontSize: 18,
+  ),
+  'body-italic': TextStyle(
+    fontFamily: 'Noto Sans',
+    fontWeight: FontWeight.w400,
+    fontStyle: FontStyle.italic,
+    fontSize: 18,
   )
 };
