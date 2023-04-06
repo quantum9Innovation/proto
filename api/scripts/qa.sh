00b593b08f45bb775af6cbc269c43860a308f5c4
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-04-06 12:33:05 -0700

    Check frontend types on lint

--- api/scripts/qa.sh
+++ api/scripts/qa.sh
@@ -2,6 +2,9 @@
 echo 'Checking types'
 echo '=============='
 yarn run tsc --noEmit -p tsconfig.json || exit 1
+echo $'\nChecking frontend types'
+echo '======================='
+yarn run tsc --noEmit -p frontend/src/tsconfig.json || exit 1
 echo $'\nChecking with `eslint`'
 echo '======================'
 yarn run eslint .
