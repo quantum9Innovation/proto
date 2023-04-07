29474bb812e971871266ca4bc533e64be9e2ee9d
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-04-07 16:22:31 -0700

    Finish VFS frontend logic

--- api/frontend/src/build.ts
+++ api/frontend/src/build.ts
@@ -32,17 +32,151 @@ const putFiles = (dirs: string[], docs: string[]) => {
   const filePreview = document.getElementById('file-preview')
   if (filePreview === null) return
   for (const dir of dirs) {
+    if (dir === 'purged') continue
+
     const folder = document.createElement('div')
     folder.className = 'item folder'
-    folder.innerText = dir
+    const folderText = document.createElement('span')
+    folderText.innerText = dir
+    const folderDel = document.createElement('button')
+    folderDel.className = 'delete'
+    folderDel.innerText = 'Delete'
+    folderDel.addEventListener('click', e => {
+      rm(dir).catch(e => { console.error(e) })
+    })
+
+    folder.appendChild(folderText)
+    folder.appendChild(folderDel)
     filePreview.appendChild(folder)
-    folder.addEventListener('click', e => { cd(dir) })
+    folderText.addEventListener('click', e => { cd(dir) })
   }
   for (const doc of docs) {
-    const folder = document.createElement('div')
-    folder.className = 'item document'
-    folder.innerText = doc
-    filePreview.appendChild(folder)
+    if (doc === 'purged') continue
+
+    const file = document.createElement('div')
+    file.className = 'item document'
+    const fileText = document.createElement('span')
+    fileText.innerText = doc
+    const fileDel = document.createElement('button')
+    fileDel.className = 'delete'
+    fileDel.innerText = 'Delete'
+    fileDel.addEventListener('click', e => {
+      rm(doc).catch(e => { console.error(e) })
+    })
+
+    file.appendChild(fileText)
+    file.appendChild(fileDel)
+    filePreview.appendChild(file)
+  }
+}
+
+const makeButtonRow = () => {
+  const buttonRow = document.createElement('div')
+  buttonRow.className = 'row'
+
+  const createDocBtn = document.createElement('button')
+  createDocBtn.id = 'create-doc'
+  createDocBtn.innerText = 'Create Document'
+
+  const createFolderBtn = document.createElement('button')
+  createFolderBtn.id = 'create-folder'
+  createFolderBtn.innerText = 'Create Folder'
+
+  // Add event listeners
+  createDocBtn.addEventListener('click', e => {
+    const popup = document.getElementById('create-doc-popup')
+    if (popup === null) return
+    popup.className = 'popup show'
+  })
+  createFolderBtn.addEventListener('click', e => {
+    const popup = document.getElementById('create-folder-popup')
+    if (popup === null) return
+    popup.className = 'popup show'
+  })
+
+  buttonRow.appendChild(createDocBtn)
+  buttonRow.appendChild(createFolderBtn)
+  return buttonRow
+}
+
+const makePopups = () => {
+  // Initialize popups array
+  const popups = []
+
+  // Create document popup
+  const doc = document.createElement('div')
+  doc.id = 'create-doc-popup'
+  doc.className = 'popup hide'
+
+  // Elements
+  const docTitle = document.createElement('h2')
+  docTitle.innerText = 'Create Document'
+  const docNameField = document.createElement('input')
+  docNameField.type = 'text'
+  docNameField.id = 'create-doc-name'
+  const docClose = document.createElement('button')
+  docClose.id = 'create-doc-close'
+  docClose.className = 'close'
+  docClose.innerText = 'Close'
+  const docSubmit = document.createElement('button')
+  docSubmit.id = 'create-doc-submit'
+  docSubmit.className = 'submit'
+  docSubmit.innerText = 'Create'
+
+  // Add to popups
+  doc.appendChild(docTitle)
+  doc.appendChild(docNameField)
+  doc.appendChild(docClose)
+  doc.appendChild(docSubmit)
+  popups.push(doc)
+
+  // Create folder popup
+  const folder = document.createElement('div')
+  folder.id = 'create-folder-popup'
+  folder.className = 'popup hide'
+
+  // Elements
+  const folderTitle = document.createElement('h2')
+  folderTitle.innerText = 'Create Folder'
+  const folderNameField = document.createElement('input')
+  folderNameField.type = 'text'
+  folderNameField.id = 'create-folder-name'
+  const folderClose = document.createElement('button')
+  folderClose.id = 'create-folder-close'
+  folderClose.className = 'close'
+  folderClose.innerText = 'Close'
+  const folderSubmit = document.createElement('button')
+  folderSubmit.id = 'create-folder-submit'
+  folderSubmit.className = 'submit'
+  folderSubmit.innerText = 'Create'
+
+  // Add event listeners
+  docClose.addEventListener('click', e => { doc.className = 'popup hide' })
+  docSubmit.addEventListener('click', e => {
+    touch(docNameField.value).catch(e => { console.error(e) })
+  })
+  folderClose.addEventListener('click', e => { folder.className = 'popup hide' })
+  folderSubmit.addEventListener('click', e => {
+    mkdir(folderNameField.value).catch(e => { console.error(e) })
+  })
+
+  // Add to popups
+  folder.appendChild(folderTitle)
+  folder.appendChild(folderNameField)
+  folder.appendChild(folderClose)
+  folder.appendChild(folderSubmit)
+  popups.push(folder)
+
+  return popups
+}
+
+const hidePopups = () => {
+  const popups = document.getElementsByClassName('popup')
+  for (const popup of popups) {
+    if (popup.classList.contains('show') || !popup.classList.contains('hide')) {
+      popup.classList.remove('show')
+      popup.classList.add('hide')
+    }
   }
 }
 
@@ -53,6 +187,16 @@ const buildFiles = () => {
     makeContent()
     content = document.getElementById('content')
   }
+
+  // Append create buttons
+  const buttonRow = makeButtonRow()
+  content!.appendChild(buttonRow)
+
+  // Make popups
+  const popups = makePopups()
+  for (const popup of popups) content!.appendChild(popup)
+
+  // Read files
   readFiles('/').then(items => {
     // Build file selector
     const dirs: string[] = items.folders
@@ -71,12 +215,7 @@ const showDir = (dir: string) => {
     // Build file selector
     const dirs: string[] = items.folders
     const docs: string[] = items.documents
-    const subdirs = dir.split('/')
-    let traversals = subdirs.length - 2
-    for (const subdir of subdirs) {
-      if (subdir === '..') traversals -= 2
-    }
-    if (traversals > 0) dirs.splice(0, 0, '..')
+    if (dir !== '/') dirs.splice(0, 0, '..')
     putFiles(dirs, docs)
   }).catch(e => { console.error(e) })
 }
@@ -85,6 +224,7 @@ window.exports = {
   setTitle,
   reset,
   makeContent,
+  hidePopups,
   buildFiles,
   showDir
 }
--- api/frontend/src/index.ts
+++ api/frontend/src/index.ts
@@ -6,16 +6,105 @@ setTitle('Files')
 makeContent()
 buildFiles()
 
+// Helper funcs
+const traverse = (dir: string) => {
+  const paths = dir.split('/')
+  paths.splice(paths.length - 2, 2)
+  return paths.join('/') + '/'
+}
+const sanitize = (dirname: string) => dirname.replace(/[^a-z0-9]/gi, '-')
+
 // Define callables
 const cd = (dir: string) => {
   // Change directory
-  const content = document.getElementById('content')
-  if (content === null) return
-  pwd += dir + '/'
+  if (dir !== '..') pwd += dir + '/'
+  else pwd = traverse(pwd)
+  hidePopups()
   showDir(pwd)
+}
+
+const mkdir = async (dir: string) => {
+  // Make directory
+  const dirname = sanitize(dir)
+  if (dirname === 'purged') {
+    alert('This is a reserved name')
+    return
+  }
+  const loc = pwd + dirname
+  const res = await fetch('/api/vfs/folder', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ loc })
+  })
+
+  // Error handling
+  if (res.status !== 200) {
+    const e = await res.text()
+    console.error(e)
+    alert(e)
+  }
+
+  // Refresh view
+  hidePopups()
+  showDir(pwd)
+}
+
+const touch = async (file: string) => {
+  // Create document
+  const filename = sanitize(file)
+  if (filename === 'purged') {
+    alert('This is a reserved name')
+    return
+  }
+  const content = {}
+  const res = await fetch('/api/vfs/document', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ loc: pwd, name: filename, content })
+  })
+
+  // Error handling
+  if (res.status !== 200) {
+    const e = await res.text()
+    console.error(e)
+    alert(e)
+  }
+
+  // Refresh view
+  hidePopups()
+  showDir(pwd)
+}
+
+const rm = async (name: string) => {
+  // Delete resource
+  let loc = pwd + name
+  if (name === '..') loc = pwd
+  const res = await fetch(
+    '/api/vfs/resource?'
+    + new URLSearchParams({ loc }).toString(),
+    { method: 'DELETE' }
+  )
+
+  // Error handling
+  if (res.status !== 200) {
+    const e = await res.text()
+    console.error(e)
+    alert(e)
+  }
+
+  // Refresh view
+  if (name === '..') cd(name)
+  else showDir(pwd)
 }
 
 // Export callables
 window.exports = {
-  cd
+  cd,
+  mkdir,
+  touch,
+  rm
 }
