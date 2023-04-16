dfeb1882b0a3d16f200c3076580935e5b8b631cb
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-04-16 13:45:59 -0700

    Specify that the frontend is built twice
    The frontend is compiled within the main API to allow for ESLint error detection and also as its own separate module for web deployments

--- api/scripts/build.sh
+++ api/scripts/build.sh
@@ -3,7 +3,7 @@ echo 'Building API from .'
 yarn run tsc -p tsconfig.json
 echo 'API built to compiled/'
 echo '======================'
-echo 'Building frontend from frontend/src/'
+echo 'Rebuilding frontend from frontend/src/'
 yarn run tsc -p frontend/src/tsconfig.json
 echo 'Frontend built to frontend/webjs/'
 echo '================================='
