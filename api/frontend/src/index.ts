19329d75f16ed534185b382d55dc2380f6e6c699
q9i <46249765+quantum9Innovation@users.noreply.github.com>
2023-06-11 18:10:22 -0700

    Remove unnecessary Noto Sans Ethiopic font
    This font is already included in the universal Noto Sans bundle

--- api/frontend/src/index.ts
+++ api/frontend/src/index.ts
@@ -143,11 +143,7 @@ const konami = () => {
 const register = async () => {
   // Register fonts
   const notoSans = new FontFace('Noto Sans', 'url(frontend/assets/noto-universal.ttf)')
-  const notoSansEthiopic = new FontFace(
-    'Noto Sans Ethiopic', 'url(frontend/assets/noto-ethiopic.ttf)'
-  )
   document.fonts.add(notoSans)
-  document.fonts.add(notoSansEthiopic)
 
   const sent = Date.now()
   const res = await fetch('/api/init/session', {
