13bacdc51173552074a7d112e0d05b9f922c3e0e
Ananth Venkatesh <46249765+quantum9Innovation@users.noreply.github.com>
2024-05-25 21:34:14 -0700

    chore: switch from yarn to pnpm (#117)

--- api/scripts/build.sh
+++ api/scripts/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 echo 'Building API from .'
-yarn run tsc -p tsconfig.json
+pnpm tsc -p tsconfig.json
 echo 'API built to compiled/'
 echo '======================'
 echo 'Rebuilding frontend from frontend/src/'
-yarn run tsc -p frontend/src/tsconfig.json
+pnpm tsc -p frontend/src/tsconfig.json
 echo 'Frontend built to frontend/webjs/'
 echo '================================='
